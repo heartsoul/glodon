202155954513b751fdbfb385c0cd0be5cf6f12be
张金波(10023977) <zhangjb-f@glodon.com>
2018-05-10 11:00:01 +0800

    [feat:优化] 1.代码整理

--- src/app/estate/src/common/constant/server-config.js
+++ src/app/estate/src/common/constant/server-config.js
@@ -6,13 +6,13 @@
 
 // 服务器地址
 
-// // 测试服务器
+// 测试服务器
 export const SERVER_TYPE = "TEST"
 export const BASE_UPLOAD_URL =  "http://172.16.233.183:8093";//图片服务
 export const BASE_URL = "http://10.1.83.30"; //应用服务
 export const BASE_URL_BLUEPRINT_TOKEN = BASE_URL+"/app.html?param=";//图纸的url地址
 
-// // 预生产服务器
+// 预生产服务器
 // export const SERVER_TYPE = "PRERELEASE"
 // export const BASE_UPLOAD_URL =  "https://api.glodon.com/nss";//图片服务
 // export const BASE_URL = "http://47.95.204.243"; // 应用服务
