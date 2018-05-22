11c4a2d6a7cfae3f5e01879e95630a013b6213ad
周瑞峰 <zhourf@glodon.com>
2018-05-22 15:26:46 +0800

    增加下载模型离线的方法

--- src/app/estate/src/pages/offline/download/DownloadModel.js
+++ src/app/estate/src/pages/offline/download/DownloadModel.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import RNFS from 'react-native-fs';
+// import {} from '../../../node_modules/app-html/app.html'
 
 import {
     Platform,
@@ -10,14 +11,11 @@ import {
   } from 'react-native';
 
   import { zip, unzip, unzipAssets, subscribe } from 'react-native-zip-archive';
+  import {Buffer} from 'buffer';
 
-
-  const instructions = Platform.select({
-    ios: 'Press Cmd+R to reload,\n' +
-      'Cmd+D or shake for dev menu',
-    android: 'Double tap R on your keyboard to reload,\n' +
-      'Shake or press menu button for dev menu',
-  });
+  const appKey = 'wAL3NLP0aplEc6GipAXyTbXm7yENchzk';
+  const appSecret = 'XV2tDlHB7aBVpEzHLdgcSspABjGWjIMR';
+  
   
   export default class App extends Component {
 
@@ -29,10 +27,105 @@ import {
     }
 
     
+    //获取离线下载的token
+    getToken = ()=>{
+        let key = appKey+':'+appSecret;
+        let encodeKey = new Buffer(key).toString('base64');
+        let auth = 'Basic '+encodeKey;
+        let url = 'https://api.bimface.com/oauth2/token';
+        let ops = {
+            method:'POST',
+            headers: {
+                'Accept': 'application/json',
+                "Content-Type": "application/json;charset=utf-8",
+                "X-Requested-With": "XMLHttpRequest",
+                "Authorization":auth,
+            },
+        };
 
+        fetch(url,ops).then((response) => response.json())
+        .then((responseJson) => {
+            // { code: 'success',
+            //     message: null,
+            //     data:
+            //     { expireTime: '2018-05-29 08:38:49',
+            //     token: '6532e462-334b-48dd-a2f3-86c7b1ab2317' } }
+          console.log(responseJson);
+          this.createOffline(responseJson.data.token);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+
+    //生成离线包
+    createOffline=(token)=>{
+        let fileId = '1353300132668256';
+        let url = `https://api.bimface.com/files/${fileId}/offlineDatabag`;
+        let auth = 'bearer '+token;
+        let ops = {
+            method:'PUT',
+            headers:{
+                'Accept': 'application/json',
+                "Content-Type": "application/json;charset=utf-8",
+                "X-Requested-With": "XMLHttpRequest",
+                "Authorization":auth,
+            },
+        }
+        fetch(url,ops).then((response)=>response.json())
+        .then((responseJson)=>{
+            // { code: 'success',
+            //     message: null,
+            //     data:
+            //     { createTime: '2018-05-22 09:28:23',
+            //     databagVersion: '3.0',
+            //     fileId: 1353300132668256,
+            //     reason: null,
+            //     status: 'success' } }
+            console.log(responseJson);
+            this.getAddress(responseJson.data.fileId,responseJson.data.databagVersion,token);
+        }).catch((error) => {
+            console.error(error);
+          });
+    }
+
+    //获取离线包地址
+    getAddress = (fileId,databagVersion,token)=>{
+        let url = `https://api.bimface.com/data/databag/downloadUrl?fileId=${fileId}&type=offline&databagVersion=${databagVersion}`;
+        let auth = 'bearer '+token;
+        let ops = {
+            method:'GET',
+            headers:{
+                'Accept': 'application/json',
+                "Content-Type": "application/json;charset=utf-8",
+                "X-Requested-With": "XMLHttpRequest",
+                "Authorization":auth,
+            },
+        }
+        fetch(url,ops).then((response)=>response.json())
+        .then((responseJson)=>{
+            // { code: 'success',
+            //     message: null,
+            //     data: 'http://bf-prod-databag.oss-cn-beijing.aliyuncs.com/1e5345adce95ee35646148ffaa6194e1/1e5345adce95ee35646148ffaa6194e1.zip?Expires=1526971919&OSSAccessKeyId=5nGlEwOIzrwCVaDZ&Signature=z4DFk0dJDBRXL9sElfgAHQxBYWQ%3D' }
+            console.log(responseJson);
+            let name = this.getName(responseJson.data)
+            this.downloadFile(responseJson.data,name);
+        }).catch((error)=>{
+            console.error(error);
+        });
+    }
+
+    getName=(url)=>{
+        let index = url.indexOf('?');
+        let str = url.substring(0,index);
+        let lastIndex = str.lastIndexOf('/');
+        let name = str.substring(lastIndex+1);
+        let dotIndex = name.lastIndexOf('.');
+        return name.substring(0,dotIndex);
+    }
 
     /*下载文件*/
-    downloadFile() {
+    downloadFile=(formUrl,name)=> {
         // On Android, use "RNFS.DocumentDirectoryPath" (MainBundlePath is not defined)
 
         // 图片
@@ -44,8 +137,10 @@ import {
         // const path = RNFS.ExternalStorageDirectoryPath;
         const path = '/sdcard/bimTest'
         console.log('start download  path='+path);
-        const downloadDest = `${path}/${((Math.random() * 1000) | 0)}.zip`;
-        const formUrl = 'http://files.cnblogs.com/zhuqil/UIWebViewDemo.zip';
+        const downloadDest = `${path}/${name}.zip`;
+        console.log('downloadDest='+downloadDest);
+        // const downloadDest = `${path}/${((Math.random() * 1000) | 0)}.zip`;
+        // const formUrl = 'http://files.cnblogs.com/zhuqil/UIWebViewDemo.zip';
 
         // 视频
         // const downloadDest = `${RNFS.MainBundlePath}/${((Math.random() * 1000) | 0)}.mp4`;
@@ -117,6 +212,8 @@ import {
                 unzip(sourcePath, targetPath)
                 .then((address) => {
                     console.log(`unzip completed at ${address}`)
+                    const downloadDest = `${path}/${name}/app.html`;
+                    this.copyAppHtml(downloadDest);
                 })
                 .catch((error) => {
                     console.log(error)
@@ -133,6 +230,65 @@ import {
         }
 
     }
+
+    //复制app.html 到离线包目录下
+    copyAppHtml=(path)=>{
+        const src = 'app.html';
+        RNFS.readFileAssets(src)
+        .then((result)=>{
+            console.log('read success result='+result);
+            this.writeFile(path,result);
+        }).catch((err)=>{
+            console.log('read error'+err.message);
+        })
+    }
+
+    writeFile=(path,content)=> {
+
+        // write the file
+        RNFS.writeFile(path, content, 'utf8')
+            .then((success) => {
+                console.log('write success', path);
+            })
+            .catch((err) => {
+                console.log('write error'+err.message);
+            });
+    }
+
+    /*读取txt文件内容*/
+    readFile() {
+        // create a path you want to delete
+        const path = RNFS.MainBundlePath + '/test.txt';
+
+        return RNFS.readFile(path)
+            .then((result) => {
+                console.log(result);
+
+                this.setState({
+                    readTxtResult: result,
+                })
+            })
+            .catch((err) => {
+                console.log(err.message);
+
+            });
+    }
+
+    /*将文本写入本地 txt*/
+    // writeFile() {
+    //     // create a path you want to write to
+    //     const path = RNFS.MainBundlePath + '/test.txt';
+
+    //     // write the file
+    //     RNFS.writeFile(path, '这是一段文本，YES', 'utf8')
+    //         .then((success) => {
+    //             console.log('path', path);
+    //         })
+    //         .catch((err) => {
+    //             console.log(err.message);
+    //         });
+    // }
+
 
     /*删除文件*/
     deleteFile() {
@@ -152,7 +308,7 @@ import {
 
     render() {
       return (
-        <TouchableOpacity onPress={this.downloadFile}>
+        <TouchableOpacity onPress={this.getToken}>
             <View >
                 <Text style={{marginTop:40}}>
                     Welcome to React Native!
