38f01de5cdf7e77e0e4dc60c83aff8762544b723
陈文杰 <chenwj-a@glodon.com>
2018-06-11 17:25:18 +0800

    [feature:分享] 1、Android友盟分享集成

---
+++ src/app/estate/android/app/src/main/java/com/umeng/soexample/wxapi/WXEntryActivity.java
@@ -0,0 +1,11 @@
+package com.umeng.soexample.wxapi;
+
+import com.umeng.socialize.weixin.view.WXCallbackActivity;
+
+/**
+ * Created by cwj on 2018/6/11.
+ * Description:WXEntryActivity
+ */
+
+public class WXEntryActivity extends WXCallbackActivity {
+}
