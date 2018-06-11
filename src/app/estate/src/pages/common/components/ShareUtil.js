e6a3070dcabbdeb82f186cd05ff65c984cd1c248
张金波(10023977) <zhangjb-f@glodon.com>
2018-06-11 17:44:29 +0800

    [feat:fix] 1. 分享调用

--- src/app/estate/src/pages/common/components/ShareUtil.js
+++ src/app/estate/src/pages/common/components/ShareUtil.js
@@ -24,14 +24,16 @@ var { NativeModules } = require('react-native');
 // return UMSocialPlatformType_Twitter;
 // case 9:
 // return UMSocialPlatformType_WechatFavorite;
+// case 33:
+//       return UMSocialPlatformType_UnKnown; // more
+// default:
+//       return UMSocialPlatformType_QQ;
 export function share(text = '', icon='', link='', title='', platform=0, completion=(a,b)=>{}) {
     NativeModules.UMShareModule.share(text,icon,link,title,platform,completion);
 }
-export function shareboard(text = '', icon='', link='', title='', platforms=[0,1,2,3,4,5,6,9], completion=(a,b)=>{}) {
-    // alert(NativeModules.UMShareModule)
-    // alert(NativeModules.UMShareModule.shareboard)
-    // alert(NativeModules.UMShareModule.share)
+export function shareboard(text = '', icon='', link='', title='', platforms=[0,1,2,3,4,5,6,9,33], completion=(a,b)=>{}) {
     
     NativeModules.UMShareModule.shareboard(text,icon,link,title,platforms,completion);
+    // NativeModules.UMShareModule.share(text,icon,link,title,33,completion);
 }
 
